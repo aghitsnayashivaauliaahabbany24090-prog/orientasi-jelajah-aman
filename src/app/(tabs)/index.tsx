92c503a6853fcbfb0c4f083d0f4927bd9522aacd
Aghitsna Yashiva Aulia Ahabbany <aghitsna.yashiva.aulia.ahabbany24090@mhs.uingusdur.ac.id>
2026-09-22 09:14:18 +0700

    chore: restrukturisasi folder untuk Expo Router

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,12 +1,11 @@
 // app/index.tsx
-import { Platform, View } from "react-native";
+import { View } from "react-native";
 import { useState, useEffect } from "react";
 // Catatan: gunakan ../components/ jika file berada di dalam folder app/
 // yang sejajar dengan folder components/
-import WeatherCard from "../components/WeatherCard";
-import SearchBox from "../components/SearchBox";
-import RiwayatList from "../components/RiwayatList";
-import IndikatorAQI from "../components/IndikatorAQI";
+import WeatherCard from "../../components/WeatherCard";
+import SearchBox from "../../components/SearchBox";
+import RiwayatList from "../../components/RiwayatList";
 export default function HalamanUtama() {
 const [kotaAktif, setKotaAktif] = useState("Pekalongan");
 const [riwayat, setRiwayat] = useState<string[]>(["Pekalongan"]);
@@ -20,12 +19,10 @@ if (!riwayat.includes(kota)) {
 setRiwayat([...riwayat, kota]);
 }
 }
-const paddingTop = Platform.OS === "web" ? 80 : 16;
 return (
-<View style={{ padding: 16, gap: 16, paddingTop }}>
+<View style={{ padding: 16,paddingTop: 70, gap: 16 }}>
 <SearchBox onCari={handleCari} />
 <WeatherCard kota={kotaAktif} suhu={29} tingkatAQI="BAIK" />
-<IndikatorAQI laporan={{ kota: kotaAktif, indeksAQI: 50, tingkat: "BAIK" }} />
 <RiwayatList daftarKota={riwayat} />
 </View>
 );
